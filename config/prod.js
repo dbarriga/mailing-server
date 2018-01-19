13b2611adc4380789ff25140e776e23ec2b8ce66
dbarriga <dbarriga@intersysconsulting.com>
2018-01-19 17:54:24 -0600

    charge, stripe, envs

--- config/prod.js
+++ config/prod.js
@@ -4,5 +4,7 @@ module.exports = {
   mongoURI: process.env.MONGO_URI,
   cookieKey: process.env.COOKIE_KEY,
   outlookPassword: process.env.OUTLOOK_PASSWORD,
-  outlookApplicationId: process.env.OUTLOOK_APPLICATION_ID
+  outlookApplicationId: process.env.OUTLOOK_APPLICATION_ID,
+  stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+  stripeSecretKey: process.env.STRIPE_SECRET_KEY
 };
