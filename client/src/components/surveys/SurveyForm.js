717cddde85109ef6f2d4bc8fe3cd2bc58511e5e7
dbarriga <dbarriga@intersysconsulting.com>
2018-01-24 15:05:48 -0600

    buttons and style in survey form

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -2,6 +2,7 @@ import _ from 'lodash';
 import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
 import SurveyField from './SurveyField';
+import { Link } from 'react-router-dom';
 
 const FIELDS = [
   { label: 'Survey Title', name: 'title' },
@@ -12,7 +13,7 @@ const FIELDS = [
 
 class SurveyForm extends Component {
   renderFields() {
-    return _.map(FIELDS, ({label, name}) => {
+    return _.map(FIELDS, ({ label, name }) => {
       return (
         <Field
           component={SurveyField}
@@ -30,7 +31,13 @@ class SurveyForm extends Component {
       <div>
         <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
           {this.renderFields()}
-          <button type="submit">Submit</button>
+          <Link to="/surveys" className="red btn-flat white-text">
+          Cancel
+          </Link>
+          <button type="submit" className="teal btn-flat right white-text">
+            Next
+            <i className="material-icons right">done</i>
+          </button>
         </form>
       </div>
     );
